deeed6611016f8d1022ff17e18655af4fe4b661c
ShujinW <82189631+ShujinW@users.noreply.github.com>
2022-02-15 17:03:14 +0800

    Delete Deep_video_inpainting_localization directory

--- Deep_video_inpainting_localization/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-import utils.losses
-import utils.metrics
-import utils.read_dataset
-import utils.tee_print
-import utils.TensorflowUtils
-import utils.bilinear_upsample_weights
-import utils.vgg_mfcn
